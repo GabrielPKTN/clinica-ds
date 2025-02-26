dc16f54afd0a485f5161e678aef5b98b8aa4aed9
GABRIEL LACERDA CORREIA <25132902@CFP127ALUNO.LOCAL>
2025-02-26 11:24:45 -0300

    Métodos get e set iniciados com cálculo do IMC

--- src/br/senai/sp/jandira/clinica/Clinica.java
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -7,20 +7,18 @@ import br.senai.sp.jandira.clinica.model.Paciente;
 public class Clinica {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		
 		// Criando um objeto paciente
 		
 		Paciente p1 = new Paciente();
-		p1.nome = "Joanna";
-		p1.altura = 1.64;
+		p1.setNome("Joanna"); 
+		p1.setAltura(1.64);
 		p1.peso = 59;
 		p1.numeroTelefone = "11 94449 2067";
 		p1.dataNascimento = LocalDate.of(2007, 7, 3);
 		
 		Paciente p2 = new Paciente();
-		p2.nome = "Gabriel";
-		p2.altura = 1.74;
+		p2.setNome("Gabriel");
+		p2.setAltura(1.78);
 		p2.peso = 78;
 		p2.numeroTelefone = "11 94449 2067";
 		p2.dataNascimento = LocalDate.of(2005, 5, 10);
--- src/br/senai/sp/jandira/clinica/model/Paciente.java
+++ src/br/senai/sp/jandira/clinica/model/Paciente.java
@@ -1,19 +1,49 @@
+/*
+ * Programador: Gabriel Lacerda Correia
+ * Data: 25/02/2025
+ * Empresa: Senai-Jandira
+ */
+
 package br.senai.sp.jandira.clinica.model;
 
 import java.time.LocalDate;
 
 public class Paciente {
-	public String nome;
+	private String nome;
 	public LocalDate dataNascimento;
 	public int peso;
-	public double altura;
+	private double altura;
 	public String numeroTelefone;
+	private double imc;
+	
+	public void setAltura(double altura) {
+		if(altura > 0) {
+			//TRUE
+			this.altura = altura;
+		} else {
+			//FALSE
+			System.out.println("A altura do(a) paciente "+ nome +" é invalida!\nA altura precisa ser maior que zero.");
+		}
+	}
+	
+	public void setNome(String nome) {
+		if(nome.trim().length() >= 3) {
+			//TRUE
+			this.nome = nome.toUpperCase();
+		} else {
+			//FALSE
+			System.out.println("O nome " + nome + ", não é valido!\nO nome deve conter pelo menos 3 caracteres.");
+		}
+		
+		
+	}
 	
 	public void calcularIdade() {
 		
 	}
-	public void calcularImc() {
-		
+	private double calcularImc() {
+		imc = peso/Math.pow(altura, 2);
+		return imc;
 	}
 	public void classificarImc() {
 		
@@ -24,6 +54,8 @@ public class Paciente {
 		System.out.println("================");
 		System.out.println("NOME: " + nome);
 		System.out.println("PESO: " + peso);
+		System.out.println("ALTURA: " + altura);
+		System.out.println("IMC: " + String.format("%.2f", calcularImc()));
 		System.out.println("TELEFONE: " + numeroTelefone);
 		System.out.println("================");
 	}
